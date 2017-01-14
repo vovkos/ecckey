d68f1330016d620fef97710b462287698dc691b7
Vladimir Gladkov <vovkos@gmail.com>
2017-01-14 15:26:36 +0800

    [travis] added osx build

---
+++ travis/install-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CMAKE_VERSION=3.3.2
+CMAKE_VERSION_DIR=v3.3
+CMAKE_OS=Linux-x86_64
+CMAKE_TAR=cmake-$CMAKE_VERSION-$CMAKE_OS.tar.gz
+CMAKE_URL=http://www.cmake.org/files/$CMAKE_VERSION_DIR/$CMAKE_TAR
+CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
+
+wget --quiet $CMAKE_URL
+mkdir -p $CMAKE_DIR
+tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
+export PATH=$CMAKE_DIR/bin:$PATH
