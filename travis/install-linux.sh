d6454b5716aa597c1a556ad8cd955bb68ee3b161
Vladimir Gladkov <vovkos@gmail.com>
2017-01-15 14:23:27 +0800

    [travis] added x86/amd64 x Debug/Release to build matrix

--- travis/install-linux.sh
+++ travis/install-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# manually install CMake -- we need at least CMake 3.3
+
 CMAKE_VERSION=3.3.2
 CMAKE_VERSION_DIR=v3.3
 CMAKE_OS=Linux-x86_64
@@ -11,3 +13,23 @@ wget --quiet $CMAKE_URL
 mkdir -p $CMAKE_DIR
 tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
+
+# now to official APT packages
+
+if [ "$TARGET_CPU" == "x86" ]; then
+	sudo dpkg --add-architecture i386
+	sudo apt-get -qq update
+
+	# OpenSSL is already installed, but 64-bit only
+
+	sudo apt-get install -y libssl-dev:i386
+
+	# install g++-multilib -- in the end, after i386 packages!
+
+	sudo apt-get install -y g++-multilib
+
+	# CMake fails to properly switch between 32-bit and 64-bit libraries on Ubuntu
+
+	echo "set (OPENSSL_LIB_DIR /usr/lib/i386-linux-gnu)" >> paths.cmake
+	echo "set (EXPAT_INC_DIR DISABLED)" >> paths.cmake
+fi
