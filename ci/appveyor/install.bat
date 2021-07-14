c7a4aba24d852b67f2cc66c6d8e9e5ca292fdc4f
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2021-07-14 23:54:14 +0800

    [ci] add github ci (github actions)

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -17,15 +17,6 @@ mkdir %DOWNLOAD_DIR%
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
-:: Ragel
-
-mkdir %DOWNLOAD_DIR%\ragel
-appveyor DownloadFile %RAGEL_DOWNLOAD_URL% -FileName %DOWNLOAD_DIR%\ragel.exe
-
-echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
-
-:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
 :: OpenSSL
 
 set OPENSSL_DIR_CMAKE=%OPENSSL_DIR:\=/%
---
+++ ci/github-actions/install-osx.sh
@@ -0,0 +1,18 @@
+#...............................................................................
+#
+#  This file is part of the ECCKey utility.
+#
+#  ECCKey is distributed under the MIT license.
+#  For details see accompanying license.txt file,
+#  the public copy of which is also available at:
+#  http://tibbo.com/downloads/archive/ecckey/license.txt
+#
+#...............................................................................
+
+brew update
+brew install ragel
+
+# openssl is already installed, but not linked
+
+echo "set (OPENSSL_INC_DIR /usr/local/opt/openssl/include)" >> paths.cmake
+echo "set (OPENSSL_LIB_DIR /usr/local/opt/openssl/lib)" >> paths.cmake
