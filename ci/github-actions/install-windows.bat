86a7a896eaf4ea5d64e816e03ab484d0dc538a1f
Vladimir Gladkov <vovkos@gmail.com>
2024-08-01 23:28:03 +0300

    [ci] major update:
    - use actions/checkout@v4
    - run on macos-13
    - run on ubuntu-22.04
    - use wget for file downloads
    - use a fixed version of openssl (v1.1.1.2100)

--- ci/github-actions/install-windows.bat
+++ ci/github-actions/install-windows.bat
@@ -16,31 +16,39 @@ set DOWNLOAD_DIR_CMAKE=%DOWNLOAD_DIR:\=/%
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+:: wget
+
+echo Installing wget...
+
+choco install wget --no-progress || exit
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
 :: Ragel
 
-echo Downloading Ragel...
+echo Downloading %RAGEL_DOWNLOAD_URL%...
 
 mkdir %DOWNLOAD_DIR%
-powershell "Invoke-WebRequest -Uri %RAGEL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\ragel.exe"
+wget -nv %RAGEL_DOWNLOAD_URL% -O %DOWNLOAD_DIR%\ragel.exe || exit
 
-echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
+echo set(RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 :: OpenSSL
 
-echo Installing OpenSSL...
+echo Installing OpenSSL %OPENSSL_VERSION%...
 
-choco install openssl --no-progress %CHOCO_PLATFORM%
+choco install openssl --version=%OPENSSL_VERSION% --no-progress %CHOCO_PLATFORM% || exit
 
 set OPENSSL_ROOT_DIR=C:/Program Files%PROGRAM_FILES_DIR_SUFFIX%/OpenSSL%OPENSSL_DIR_SUFFIX%
 
-echo set (OPENSSL_INC_DIR "%OPENSSL_ROOT_DIR%/include") >> paths.cmake
-echo set (OPENSSL_LIB_DIR "%OPENSSL_ROOT_DIR%/lib") >> paths.cmake
-echo set (OPENSSL_DLL_DIR "%OPENSSL_ROOT_DIR%/bin") >> paths.cmake
-echo set (OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
-echo set (OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
-echo set (OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
-echo set (OPENSSL_SSL_DLL_NAME libssl%OPENSSL_DLL_SUFFIX%) >> paths.cmake
+echo set(OPENSSL_INC_DIR "%OPENSSL_ROOT_DIR%/include") >> paths.cmake
+echo set(OPENSSL_LIB_DIR "%OPENSSL_ROOT_DIR%/lib") >> paths.cmake
+echo set(OPENSSL_DLL_DIR "%OPENSSL_ROOT_DIR%/bin") >> paths.cmake
+echo set(OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
+echo set(OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
+echo set(OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
+echo set(OPENSSL_SSL_DLL_NAME libssl%OPENSSL_DLL_SUFFIX%) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
