0346aad9de140e55271f77485162d0c53cc7f412
Vladimir Gladkov (laptop2-arch) <vovkos@gmail.com>
2025-12-14 13:02:40 +0800

    [ci] use brew --prefix (macos amd64 & macos arm64 use different prefixes)

--- ci/github-actions/install-osx.sh
+++ ci/github-actions/install-osx.sh
@@ -11,7 +11,9 @@
 
 brew install ragel
 
+BREW_PREFIX=$(brew --prefix)
+
 # openssl is already installed, but not linked
 
-echo "set (OPENSSL_INC_DIR /usr/local/opt/openssl/include)" >> paths.cmake
-echo "set (OPENSSL_LIB_DIR /usr/local/opt/openssl/lib)" >> paths.cmake
+echo "set (OPENSSL_INC_DIR $BREW_PREFIX/opt/openssl/include)" >> paths.cmake
+echo "set (OPENSSL_LIB_DIR $BREW_PREFIX/opt/openssl/lib)" >> paths.cmake
